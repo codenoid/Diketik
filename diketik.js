352aa9a49bb076edd263b4ceff60ea1e2004971b
codenoid <14269809+codenoid@users.noreply.github.com>
2019-08-14 05:35:20 +0700

    format code, direct-write if ms is 0, update docs

--- diketik.js
+++ diketik.js
@@ -1,51 +1,55 @@
 // github.com/codenoid/Diketik
 const Diketik = el => ({
-  	el,
-  	sleep: (ms) => {
-  		return new Promise(resolve => setTimeout(resolve, ms))
-  	},
-  	isIterable: (obj) => {
-	  	// checks for null and undefined
-	  	if (obj == null) {
-	    	return false;
-	  	}
-	  	return typeof obj[Symbol.iterator] === 'function';
-	},
-  	ticker: async (value, ms = 250, erase = false) => {
-  		if ( el != null ) {
+    el,
+    sleep: (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    },
+    isIterable: (obj) => {
+        // checks for null and undefined
+        if (obj == null) {
+            return false;
+        }
+        return typeof obj[Symbol.iterator] === 'function';
+    },
+    ticker: async (value, ms = 250, erase = false) => {
+        if (el != null) {
 
-  			if ( erase == true ) {
-  				el.innerHTML = ""
-  			}
+            if (erase == true) {
+                el.innerHTML = ""
+            }
 
-	  		if ( typeof(value) == "string" && value.length > 0 ) {
-	  			// convert string into array of character
-	  			// "abc def " => ["a", "b", "c", " ", "d", "e", "f", " "]
-	  			value = value.split("")
+            if (typeof(value) == "string" && value.length > 0) {
+                // convert string into array of character
+                // "abc def " => ["a", "b", "c", " ", "d", "e", "f", " "]
+                array = value.split("")
 
-				for (var i = 0; i < value.length; i++) {
-	  				el.insertAdjacentHTML('beforeend', value[i])
-	  				await Diketik(null).sleep(ms)
-				}
-	  		}
-  		} else {
-  			console.warn("Diketik.ticker element not found !");
-  		}
-  	},
-  	rolling: async (value, ms = 250) => {
-  		if ( el != null ) {
-  			if ( Diketik(0).isIterable(value) ) {
+                if (ms > 0) {
+                    for (var i = 0; i < array.length; i++) {
+                        el.insertAdjacentHTML('beforeend', array[i])
+                        await Diketik(null).sleep(ms)
+                    }
+                } else {
+                    el.insertAdjacentHTML('beforeend', value)
+                }
+            }
+        } else {
+            console.warn("Diketik.ticker element not found !");
+        }
+    },
+    rolling: async (value, ms = 250) => {
+        if (el != null) {
+            if (Diketik(0).isIterable(value)) {
 
-				for (var i = 0; i < value.length; i++) {
-	  				el.innerHTML = value[i]
-	  				await Diketik(null).sleep(ms)
-				}
+                for (var i = 0; i < value.length; i++) {
+                    el.innerHTML = value[i]
+                    await Diketik(null).sleep(ms)
+                }
 
-  			} else {
-  				console.log("Diketik.rolling(value) is not iterable.")
-  			}
-  		} else {
-  			console.warn("Diketik.rolling element not found !");
-  		}
-  	}
+            } else {
+                console.log("Diketik.rolling(value) is not iterable.")
+            }
+        } else {
+            console.warn("Diketik.rolling element not found !");
+        }
+    }
 })
